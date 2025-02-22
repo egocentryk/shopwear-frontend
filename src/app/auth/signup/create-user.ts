7c8bb080b67667031ca87d0a5ebb1beafca164ec
egocentryk <lukasz.skowron@gmail.com>
2025-02-22 09:24:16 +0100

    feat(): json payloads implemented

--- src/app/auth/signup/create-user.ts
+++ src/app/auth/signup/create-user.ts
@@ -1,19 +1,13 @@
 'use server'
 
-import { API_URL } from '@/app/constants/api'
-import { getErrorMessage } from '@/app/util/errors'
+import { post } from '@/app/util/fetch'
 import { redirect } from 'next/navigation'
 
 export default async function createUser(_prevState: any, formData: FormData) {
-  const res = await fetch(`${API_URL}/users`, {
-    method: 'POST',
-    body: formData,
-  })
+  const { error } = await post('users', formData)
 
-  const parsedRes = await res.json()
-
-  if (!res.ok) {
-    return { error: getErrorMessage(parsedRes) }
+  if (error) {
+    return { error }
   }
 
   redirect('/')
