59d066c7e94120e81f6536c5863c0e10fba83d63
egocentryk <lukasz.skowron@gmail.com>
2025-02-22 09:37:22 +0100

    feat(): login server action created

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,13 +1,22 @@
+'use client'
+
 import { Button, Link, Stack, TextField } from "@mui/material"
 import NextLink from "next/link"
+import { useActionState } from "react"
+import login from "./login"
 
 export default function LoginPage() {
+  const [state, formAction] = useActionState(login, { error: ''})
+  
   return (
-    <Stack spacing={2} className="w-full max-w-xs">
-      <TextField label="Email" variant="outlined" type="email"></TextField>
-      <TextField label="Password" variant="outlined" type="password"></TextField>
-      <Button variant="contained">Login</Button>
+    <form action={formAction} className="w-full max-w-xs">
+      <Stack spacing={2}>
+      <TextField error={!!state.error} helperText={state.error} name="email" label="Email" variant="outlined" type="email"></TextField>
+      <TextField error={!!state.error} helperText={state.error} name="password" label="Password" variant="outlined" type="password"></TextField>
+      <Button type="submit" variant="contained">Login</Button>
       <Link component={NextLink} href="/auth/signup" className="self-center">Sign Up</Link>
     </Stack>
+    </form>
+    
   );
 }
