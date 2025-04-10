09092d1369cc0cd9eca628ddf95c51a6942ed694
egocentryk <lukasz.skowron@gmail.com>
2025-04-10 12:47:45 +0200

    feat(products): products websockets implemented

--- src/app/products/products.tsx
+++ src/app/products/products.tsx
@@ -1,19 +1,10 @@
 import getProducts from "./actions/get-products"
-import { Grid } from '@mui/material'
-import Product from "./product"
+import ProductsGrid from "./products-grid"
 
 export default async function Products() {
   const response = await getProducts()
   
   const products = Array.isArray(response) ? response : []
 
-  return (
-    <Grid container spacing={3}>
-      {products.map(product => (
-        <Grid key={product.id} size={{ xs: 12, sm: 6, lg: 4 }}>
-          <Product product={product} />
-        </Grid>
-      ))}
-    </Grid>
-  )
+  return <ProductsGrid products={products} />
 }
